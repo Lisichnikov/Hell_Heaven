fbc1b9963eb27ef33c1d345b581f957e8917158a
Алексей Лисичников <alexochanel@gmail.com>
2024-02-20 12:40:00 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Unbounded", 10.7999992F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.RoyalBlue;
+            label1.Location = new Point(145, 83);
+            label1.Name = "label1";
+            label1.Size = new Size(160, 22);
+            label1.TabIndex = 0;
+            label1.Text = "Великий суд";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.CornflowerBlue;
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Unbounded", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(67, 121);
+            button1.Name = "button1";
+            button1.Size = new Size(323, 55);
+            button1.TabIndex = 1;
+            button1.Text = "Куда попадешь";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.MenuHighlight;
+            button2.BackgroundImageLayout = ImageLayout.None;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.Lime;
+            button2.Location = new Point(269, 137);
+            button2.Name = "button2";
+            button2.Size = new Size(33, 33);
+            button2.TabIndex = 3;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click_1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Unbounded", 10.7999992F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.RoyalBlue;
+            label2.Location = new Point(51, 188);
+            label2.Name = "label2";
+            label2.Size = new Size(365, 22);
+            label2.TabIndex = 2;
+            label2.Text = "Напиши процент хороших дел";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Unbounded Medium", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(145, 137);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(122, 33);
+            textBox1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources._8657101_d070f;
+            pictureBox1.Location = new Point(-5, -8);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(489, 345);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackgroundImage = Properties.Resources._72nN;
+            pictureBox2.Location = new Point(-5, -8);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(489, 345);
+            pictureBox2.TabIndex = 6;
+            pictureBox2.TabStop = false;
+            pictureBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(456, 336);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Прога типо";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Label label2;
+        private TextBox textBox1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,75 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            textBox1.Visible = false;
+            label2.Visible = false;
+            button2.Visible = false;
+            button1.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = true;
+            label2.Visible = true;
+            button2.Visible = true;
+            button1.Visible = false;
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        bool CheckAge(int age)
+        {
+            bool result = true;
+            if (age <= 50)
+            {
+                result = false;
+            }
+            return result;
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            bool resChekAge = CheckAge(Convert.ToInt32(textBox1.Text));
+            int age = Convert.ToInt32(textBox1.Text);
+            label2.Text = resChekAge ? ("Ты в аду, брат") : ("Челик, ты в раю");
+            if ( age >= 50)
+            {
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                pictureBox2.Visible = true;
+            }
+            /*if (resChekAge)
+            {
+                label2.Text = "Документы выданы";
+            }
+            else
+            {
+                label2.Text = "Не можем выдать документы";
+            }*/
+
+            /*int a;
+            a = Convert.ToInt32(textBox1.Text);
+            if (a < 18)
+            {
+                label2.Text = "Не можем выдать документы";
+                return;
+            }
+            label2.Text = "Документы выданы";*/
+        }
+    }
+}
